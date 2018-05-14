81e715ca9f9a6e24c28dfb78f5060f7632492c26
hego06 <33333619+hego06@users.noreply.github.com>
2018-05-14 14:10:41 -0500

    insert updated

--- php/guarda_boletos.php
+++ php/guarda_boletos.php
@@ -1,6 +1,6 @@
 <?php 
 
-    include ('conexion.php');
+    require_once('conexion.php');
     
     $cid_expedi = $_POST['cid_expedi'];
     $paquete = explode('|',$_POST['paquete']);
@@ -36,16 +36,21 @@
     for($i=0; $i< $numpax; $i++)
     {
         $insert 	= "INSERT INTO sboletos (numfolio, cid_expedi, paquete, fechasal, nombrepax, 
-                        obser_la, la, wspan, sabre, amadeus, notas, ttotb, fhcaptura, statusbol, statusaut, nopasaj, cid_emplea, tipotarifa) 
+                        obser_la, la, wspan, sabre, amadeus, notas, ttotb, fhcaptura, statusaut, nopasaj, cid_emplea, tipotarifa) 
                         VALUES ('BLE','$cid_expedi', '$paquete[0]', '$fsalida','$nombres[$i]', '$obser_la', '$la', 
-                        '$wspan', '$sabre', '$amadeus', '$notas', '$ttotb','$fechaHoraCaptura','1','0', '$i', '$cid_emplea', '$tipotarifa')";
+                        '$wspan', '$sabre', '$amadeus', '$notas', '$ttotb','$fechaHoraCaptura','0', '$i', '$cid_emplea', '$tipotarifa')";
         mysqli_query($conx,$insert) or die (mysqli_error($conx));
 
         $last_id = mysqli_insert_id($conx);
         $num_folio = str_pad($last_id, 4, "0", STR_PAD_LEFT);
         $update_folio = "UPDATE sboletos SET numfolio='BLE$num_folio' WHERE id ='$last_id'";
 
-        mysqli_query($conx, $update_folio) or die (mysqli_error($conx));
+        mysqli_query($conx, $update_folio) or die ("no se pudo realizar el registro");
     }
+
+     echo "<script>
+                alert('REGISTROS INSERTADOS CORRECTAMENTE');
+                window.location= '../index.php'
+            </script>";
 
 ?>
